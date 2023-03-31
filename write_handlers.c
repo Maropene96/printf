d5b914591b048ca592cb9824364bfb157052a949
FelicityRashetle20 <104845547+FelicityRashetle20@users.noreply.github.com>
2023-03-31 23:12:25 +0200

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -26,7 +26,7 @@ int handle_write_char(char c, char buffer[],
 
 	buffer[i++] = c;
 	buffer[i] = '\0';
-if (width > 1)
+        if (width > 1)
 	{
 		buffer[BUFF_SIZE - 1] = '\0';
 		for (i = 0; i < width - 1; i++)
@@ -71,7 +71,7 @@ int write_number(int is_negative, int ind, char buffer[],
 	else if (flags & F_PLUS)
 		extra_ch = '+';
 	else if (flags & F_SPACE)
-extra_ch = ' ';
+                extra_ch = ' ';
 
 	return (write_num(ind, buffer, flags, width, precision,
 		length, padd, extra_ch));
@@ -115,7 +115,7 @@ int write_num(int ind, char buffer[],
 		{
 			if (extra_c)
 				buffer[--ind] = extra_c;
-return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
+                        return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
 		}
 		else if (!(flags & F_MINUS) && padd == ' ')/* extra char to left of buff */
 		{
@@ -157,7 +157,7 @@ int write_unsgnd(int is_negative, int ind,
 	char padd = ' ';
 
 	UNUSED(is_negative);
-UNUSED(size);
+        UNUSED(size);
 
 	if (precision == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
 		return (0); /* printf(".0d", 0)  no char is printed */
